34b53fffeb07b41c3181eb732bee680723ab3f49
Rob Ranf <rob@emiyaconsulting.com>
2025-04-04 19:12:14 -0700

    project cleanup

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32f01fc57efeed0731dd4264875e005d8bffa4d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
